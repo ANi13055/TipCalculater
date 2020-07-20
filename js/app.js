ad48caf524e483737c90cdda1e4e7cb63adb3538
ANi13055 <techhub13055@gmail.com>
2020-07-20 17:47:10 +0530

    change attributes and default style

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-// TODO: Tip in % increase/decrease
+// TODO: Tip in % increase/decrease value
 const tip = document.querySelector(".tip-value");
-const increase = document.querySelector(".tip-increase");
-const decrease = document.querySelector(".tip-decrease");
+const tipIncrease = document.querySelector(".tip-increase");
+const tipDecrease = document.querySelector(".tip-decrease");
 
-increase.addEventListener("click", function (e) {
+tipIncrease.addEventListener("click", function (e) {
     tip.value++;
     e.preventDefault();
 });
 
-decrease.addEventListener("click", function (e) {
+tipDecrease.addEventListener("click", function (e) {
     if (tip.value == 0) {
         tip.value -= false;
     }
@@ -16,19 +16,19 @@ decrease.addEventListener("click", function (e) {
         tip.value--;
     }
     e.preventDefault();
-})
+});
 
 // TODO: Number of People counter
 const inputBox = document.querySelector(".people-count");
-const plusValue = document.querySelector(".plus-btn");
-const minusValue = document.querySelector(".minus-btn");
+const increasePeople = document.querySelector(".people-increase");
+const decreasePeople = document.querySelector(".people-decrease");
 
-plusValue.addEventListener("click", function (e) {
+increasePeople.addEventListener("click", function (e) {
     inputBox.value++;
     e.preventDefault();
 });
 
-minusValue.addEventListener("click", function (e) {
+decreasePeople.addEventListener("click", function (e) {
     if (inputBox.value == 1) {
         inputBox.value -= false;
     }
@@ -36,30 +36,32 @@ minusValue.addEventListener("click", function (e) {
         inputBox.value--;
     }
     e.preventDefault();
-    // console.log(e);
 });
 
-// TODO: Lets calculate total tip
-let generateBill = document.querySelector(".generate");
+// TODO: Lets calculate total tip bill
+let generateBill = document.querySelector(".btn-generate");
 
-// Create new bill
+// Create bill amount with tip
 generateBill.addEventListener('click', function () {
-    let billValue = document.querySelector(".amount").value;
+    let billValue = document.querySelector(".bill-value").value;
     let tipValue = document.querySelector(".tip-value").value;
-    let totalTip = document.querySelector("#tip-amount");
-    let totalBill = document.querySelector("#total-amount");
+    let totalTip = document.querySelector(".tip-amount");
+    let totalBill = document.querySelector(".total-amount");
     let peopleCount = document.querySelector(".people-count");
     let perPerson = document.querySelectorAll(".per-person");
 
     if (peopleCount.value == 1) {
         totalTip.innerText = ((billValue * tipValue) / 100) + ' $';
+
         totalBill.innerText = (parseFloat(billValue) + parseFloat(totalTip.innerText)) + ' $';
     }
     else {
-        totalTip.innerText = (((billValue * tipValue) / 100) / (peopleCount.value)) + ' $';
-        totalBill.innerText = ((parseFloat(billValue) + parseFloat(totalTip.innerText)) / (peopleCount.value)) + ' $';
+        totalTip.innerText = ((billValue * tipValue) / 100) / (peopleCount.value) + ' $';
 
-        perPerson[0].innerText = '(Per Person)';
-        perPerson[1].innerText = '(Per Person)';
+        totalBill.innerText = (parseFloat(billValue) + parseFloat(totalTip.innerText) / (peopleCount.value)) + ' $';
+
+        for (i = 0; i < perPerson.length; i++) {
+            perPerson[i].innerText = '(Per Person)'
+        }
     }
 });
